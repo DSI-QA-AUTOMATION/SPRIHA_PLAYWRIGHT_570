68ebb59bd126583f7e8c6bb147acced88fc91158
SprihaZannat962 <gul.zannat@dsinnovators.com>
2025-10-08 18:00:44 +0600

    Update checkbox test

--- tests/Checkboxes.spec.js
+++ tests/Checkboxes.spec.js
@@ -2,12 +2,38 @@ const { test , expect } = require ('@playwright/test')
 
 test('Test checkboxes', async ({page})=>{
     await page.goto('https://demoqa.com/checkbox')
-    await page.waitForTimeout(3000);
 
-    // Single checkbox
-    await page.locator('.rct-title').check();
-    await expect(page.locator('.rct-title')).toBeChecked();
 
+    // Check the Home folder
+    await page.locator('label[for="tree-node-home"]').check();
+    await expect(page.locator('#tree-node-home')).toBeChecked();
+
+    // Expand the Home folder
+    await page.locator('(//button[@title="Toggle"])[1]').click();
+
+    // Assert sub-checkboxes
+    await expect(page.locator('#tree-node-documents')).toBeChecked();
+    await expect(page.locator('#tree-node-downloads')).toBeChecked();
+    await expect(page.locator('#tree-node-desktop')).toBeChecked();
+
+    // Uncheck the Home folder
+    await page.locator('label[for="tree-node-home"]').uncheck();
+    await expect(page.locator('#tree-node-home')).not.toBeChecked();
+
+
+    // Expand the Documents folder
+    await page.locator('label:has-text("Documents")').locator('..').locator('button[title="Toggle"]').click();
     
+    // Expand the Office folder
+    await page.locator('label:has-text("Office")').locator('..').locator('button[title="Toggle"]').click();
+
+    await page.locator('label[for="tree-node-public"]').check();
+    await page.locator('label[for="tree-node-private"]').check();
+    await page.locator('label[for="tree-node-classified"]').check();
+    await page.locator('label[for="tree-node-general"]').check();
+
+    // Assert Home checkbox
+    await expect(page.locator('#tree-node-office')).toBeChecked();
+
 
 })
