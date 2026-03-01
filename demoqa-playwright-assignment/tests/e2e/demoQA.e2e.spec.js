57c8b9996de631ebbaa9fcbd65e75871ac484662
SprihaZannat962 <gul.zannat@dsinnovators.com>
2026-03-02 00:42:41 +0600

    Add updated tests

--- demoqa-playwright-assignment/tests/e2e/demoQA.e2e.spec.js
+++ demoqa-playwright-assignment/tests/e2e/demoQA.e2e.spec.js
@@ -1,19 +1,42 @@
 import { test, expect } from "@playwright/test";
-import { HomePage } from "../../pages/HomePage";
+import { BasePage } from "../../pages/base/BasePage";
+import { PracticeFormPage } from "../../pages/PracticeFormPage";
+import { WidgetsPage } from "../../pages/WidgetsPage";
+import { DatePickerPage } from "../../pages/DatePickerPage";
+import { InteractionsPage } from "../../pages/InteractionsPage";
+import formData from "../../test-data/formData.json";
 
-test("User can login, go to book store and logout (E2E)", async ({ page }) => {
-  const home = new HomePage(page);
-  await home.gotoLoginPage();
+test("TC-15: Complete user journey", async ({ page }) => {
+  const basePage = new BasePage(page);
+  const practiceForm = new PracticeFormPage(page);
+  const widgets = new WidgetsPage(page);
+  const datePicker = new DatePickerPage(page);
+  const interactions = new InteractionsPage(page);
+  const user = formData.practiceFormUser;
+  
+  //Go to home page
+  await basePage.goto();
 
-  await home.loginUser("sprihaTest", "Spriha@1");
-  await expect(page).toHaveURL("https://demoqa.com/profile");
+  //Practice form
+  await practiceForm.gotoPracticeForm();
+  await practiceForm.fillForm(user);
+  await practiceForm.submitForm();
+  await expect(practiceForm.modal).toContainText("Thanks for submitting the form");
 
-  await home.goToBookStore();
-  await expect(
-    page.locator(
-      "//a[normalize-space()='Learning JavaScript Design Patterns']",
-    ),
-  ).toBeVisible();
+  //Tooltips
+  await widgets.gotoToolTipsPage();
+  await widgets.verifyTooltipIsVisible();
+  await expect(widgets.page.locator('.tooltip-inner')).toHaveText('You hovered over the Button');
 
-  await home.logOut();
+  //Date picker
+  await datePicker.gotoDatePickerPage();
+  await datePicker.selectDate('2027', 'January', '14');
+  await expect(datePicker.dateInput).toHaveValue('01/14/2027');
+
+  //Drag and drop
+  await interactions.gotoDragAnDropPage();
+  await interactions.gotoDragAnDropPage();
+  await interactions.simpleDragAndDrop();
+  await expect(interactions.simpleDropBox).toHaveText("Dropped!", { timeout: 5000 });
+  
 });
